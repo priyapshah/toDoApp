e97ad127a1e76931d28eab21f98158b714c5a94f
Priya Shah <shahpriya890@gmail.com>
2020-07-21 22:21:19 -0400

    add individual projects

--- src/components/IndividualProject.js
+++ src/components/IndividualProject.js
@@ -1,22 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useProjectsValue, useSelectedProjectValue } from '../context';
-import {firebase} from '../firebase';
+import { firebase } from '../firebase';
+import { FaTrashAlt } from 'react-icons/fa';
 
-export const IndividualProject = ({project}) => {
+export const IndividualProject = ({ project }) => {
     const [showConfirm, setShowConfirm] = useState(false);
-    const {projects, setProjects} = useProjectsValue();
-    const {setSelectedProject} = useSelectedProjectValue();
+    const { projects, setProjects } = useProjectsValue();
+    const { setSelectedProject } = useSelectedProjectValue();
 
     const deleteProject = docId => {
         firebase
-        .firestore()
-        .collection('projects')
-        .doc(docId)
-        .delete()
-        .then(() => {
-            setProjects([...projects]);
-            setSelectedProject('INBOX');
+            .firestore()
+            .collection('projects')
+            .doc(docId)
+            .delete()
+            .then(() => {
+                setProjects([...projects]);
+                setSelectedProject('INBOX');
 
-        })
-    }
+            });
+    };
+
+    return (
+        <>
+            <span className="sidebar__dot">•</span>
+            <span className="sidebar__project-name">{project.name}</span>
+            <span 
+            className="sidebar__project-delete" 
+            onClick={() => setShowConfirm(!showConfirm)}
+            >
+                <FaTrashAlt />
+                {showConfirm && (
+                    <div className="project-delete-modal">
+                        <div className="project-delete-modal__inner">
+                            
+                        </div>
+                    </div>
+                )}
+
+            </span>
+        </>
+    )
 }
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,25 +1,118 @@
 import React, { useState } from 'react';
-import {Projects} from '../Projects';
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from 'react-icons/fa';
+import { Projects } from '../Projects';
 import { useSelectedProjectValue } from '../../context';
 
+
 export const Sidebar = () => {
-    const { setSelectedProject } = useSelectedProjectValue;
-    const {active, setActive} = useState('inbox');
-    const {showProjects, setShowProjects} = useState(true);
-    
-    return(
-    <div className="sidebar">
-        <ul className="sidebar__generic">   {/* BEM, Block Element Modifier */}
-            <li className="inbox">Inbox</li>
-            <li className="today">Today</li>
-            <li className="next_7">This Week</li>
-        </ul>
-        <div className="sidebar__middle">
-            <h2>Projects</h2>
-        </div>
+  const { setSelectedProject } = useSelectedProjectValue();
+  const [active, setActive] = useState('inbox');
+  const [showProjects, setShowProjects] = useState(true);
 
-    <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li
+          className={active === 'inbox' ? 'active' : undefined}
+        >
+          <div
+            aria-label="Show inbox tasks"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive('inbox');
+              setSelectedProject('INBOX');
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setActive('inbox');
+                setSelectedProject('INBOX');
+              }
+            }}
+          >
+            <span>
+              <FaInbox />
+            </span>
+            <span>Inbox</span>
+          </div>
+        </li>
+        <li
+          className={active === 'today' ? 'active' : undefined}
+        >
+          <div
+            data-testid="today-action"
+            aria-label="Show today's tasks"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive('today');
+              setSelectedProject('TODAY');
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setActive('today');
+                setSelectedProject('TODAY');
+              }
+            }}
+          >
+            <span>
+              <FaRegCalendar />
+            </span>
+            <span>Today</span>
+          </div>
+        </li>
+        <li
+          data-testid="next_7"
+          className={active === 'next_7' ? 'active' : undefined}
+        >
+          <div
+            aria-label="Show tasks for the next 7 days"
+            tabIndex={0}
+            role="button"
+            onClick={() => {
+              setActive('next_7');
+              setSelectedProject('NEXT_7');
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setActive('next_7');
+                setSelectedProject('NEXT_7');
+              }
+            }}
+          >
+            <span>
+              <FaRegCalendarAlt />
+            </span>
+            <span>Next 7 days</span>
+          </div>
+        </li>
+      </ul>
+      <div
+        className="sidebar__middle"
+        aria-label="Show/hide projects"
+        onClick={() => setShowProjects(!showProjects)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') setShowProjects(!showProjects);
+        }}
+        role="button"
+        tabIndex={0}
+      >
+        <span>
+          <FaChevronDown
+            className={!showProjects ? 'hidden-projects' : undefined}
+          />
+        </span>
+        <h2>Projects</h2>
+      </div>
 
-        {showProjects && <Projects />}
+      <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
+
+      {showProjects}
     </div>
-)};
+  );
+};
